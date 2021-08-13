0ccf486bac099718fc2bf1e5ccc14dcdafe5da7e
Joseph Mureithi <33313923+JoddieNgatz@users.noreply.github.com>
2021-08-13 23:35:49 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,13 @@ import { DialogComponent } from './dialog/dialog.component';
     NbCardModule,
     NbIconModule,
     NbDialogModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
